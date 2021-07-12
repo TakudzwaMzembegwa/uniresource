c0d1b0025790a038f9011f4a6f0a9ee8069b2252
TeeKay <takudzwamzembegwa@gmail.com>
2021-07-13 01:24:15 +0200

    Relocated files

---
+++ backend/src/main/java/com/uniresource/backend/security/filter/JWTAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.uniresource.backend.security.filter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uniresource.backend.domain.entity.User;
+import com.uniresource.backend.security.handler.JWTAuthenticationFailureHandler;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public static final String SECRET = "121341werw244234w25234wewerwerwer";
+    public static final long EXPIRATION_TIME = 86400000; // 1 days
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            User loginUser = new ObjectMapper().readValue(request.getInputStream(),
+                    User.class);
+            return  authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginUser.getUsername(), loginUser.getPassword()));
+        } catch (IOException e) {
+            throw new RuntimeException("Could not read request" + e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+        String token = JWT.create().withSubject(((org.springframework.security.core.userdetails.User) authResult.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(SECRET.getBytes()));
+        response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+    }
+
+}
