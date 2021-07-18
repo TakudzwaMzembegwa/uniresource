a68a15bdb383d160e993461e576b46086d7a314e
TeeKay <takudzwamzembegwa@gmail.com>
2021-07-18 03:00:20 +0200

    Redirect to login after logout

--- backend/src/main/java/com/uniresource/backend/security/handler/UserLogoutSuccessHandler.java
+++ backend/src/main/java/com/uniresource/backend/security/handler/UserLogoutSuccessHandler.java
@@ -1,15 +1,22 @@
 package com.uniresource.backend.security.handler;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.uniresource.backend.controller.UserController;
 import com.uniresource.backend.security.configuration.JWTConfig;
 import com.uniresource.backend.security.utils.JWTTokenUtils;
 
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
@@ -26,7 +33,8 @@ public class UserLogoutSuccessHandler implements LogoutSuccessHandler {
         JWTTokenUtils.addBlackList(token);
 
         SecurityContextHolder.clearContext();
-        response.setStatus(HttpStatus.OK.value());
+        String logoutLink = linkTo(UserController.class).slash("login").toUri().toURL().toString();
+        response.setHeader("Location", logoutLink);
     }
 
 }
