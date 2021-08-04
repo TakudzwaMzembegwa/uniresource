fc8d3a4f30cec42aa1c7c1a8d02710f531e54700
TeeKay <takudzwamzembegwa@gmail.com>
2021-08-04 23:16:50 +0200

    made use of Hateos, Hal-Forms

--- backend/src/main/java/com/uniresource/backend/controller/UserController.java
+++ backend/src/main/java/com/uniresource/backend/controller/UserController.java
@@ -1,12 +1,7 @@
 package com.uniresource.backend.controller;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +15,6 @@ import com.uniresource.backend.domain.dto.SigninForm;
 import com.uniresource.backend.domain.dto.UpdatePasswordRequest;
 import com.uniresource.backend.domain.dto.UpdateUserRequest;
 import com.uniresource.backend.domain.dto.UserDto;
-import com.uniresource.backend.domain.entity.User;
 import com.uniresource.backend.domain.mapper.UserMapper;
 import com.uniresource.backend.repository.UserRepository;
 import com.uniresource.backend.security.handler.UserLogoutSuccessHandler;
@@ -46,7 +40,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -70,24 +63,26 @@ public class UserController {
 	public ResponseEntity<EntityModel<SigninForm>> signin(){
 		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(linkTo(UserController.class).slash("login").withRel("login").toUri());
-		headers.setAccessControlAllowMethods(Arrays.asList(new HttpMethod[]{HttpMethod.POST}));
-		headers.setAccept(Arrays.asList(new MediaType[]{MediaType.APPLICATION_JSON}));
+		headers.setAccessControlAllowMethods(List.of(HttpMethod.POST));
+		headers.setAccept(List.of(MediaType.APPLICATION_JSON));
 		return ResponseEntity.ok()
 				.headers(headers)
 				.body(EntityModel.of(new SigninForm(), 
 					linkTo(methodOn(UserController.class).signin()).withSelfRel()
 						.andAffordance(afford(methodOn(UserController.class).signup(null, new CreateUserRequest()))),
-					linkTo(methodOn(RootController.class).root()).withRel("home")
+					linkTo(methodOn(RootController.class).root(null)).withRel("home")
 				));
 }
 
 	@GetMapping("/signup")
 	public ResponseEntity<EntityModel<CreateUserRequest>> signup() {
-		EntityModel<CreateUserRequest> model = EntityModel.of(new CreateUserRequest());
-		model.add(linkTo(methodOn(UserController.class).signup()).withSelfRel());
-		model.add(linkTo(methodOn(UserController.class).signin()).withRel("signin"));
-		model.add(linkTo(methodOn(RootController.class).root()).withRel("home"));
-		return ResponseEntity.ok(model);
+	
+		return ResponseEntity
+				.ok(EntityModel.of(new CreateUserRequest(),
+					linkTo(methodOn(UserController.class).signup()).withSelfRel(),
+					linkTo(methodOn(UserController.class).signin()).withRel("signin"),
+					linkTo(methodOn(RootController.class).root(null)).withRel("home")
+				));
 	}
 
 	@PostMapping(path = "/signup", consumes = "multipart/form-data")
@@ -102,47 +97,45 @@ public class UserController {
 
 	@GetMapping("/update")
 	public ResponseEntity<EntityModel<UpdateUserRequest>> updateUser(Authentication authentication) {
-		
 		return ResponseEntity
 				.ok(EntityModel.of(new UpdateUserRequest(),
 					linkTo(methodOn(UserController.class).updateUser(null)).withSelfRel(),
 					linkTo(methodOn(UserController.class).getUser(null, authentication.getName())).withRel("userDetails"),
 					linkTo(methodOn(UserController.class).updatePassword(null)).withRel("changePassword"),
 					linkTo(methodOn(UserController.class).deleteUser(null)).withRel("deleteUser"),
-					linkTo(methodOn(RootController.class).root()).withRel("home"),
+					linkTo(methodOn(RootController.class).root(null)).withRel("home"),
 					linkTo(UserController.class).slash("logout").withRel("logout")
 				));
 	}
 
 	@PostMapping(path = "/update", consumes = "multipart/form-data")
-	public ResponseEntity<EntityModel<UserDto>> updateUser(@RequestPart("file") @Valid MultipartFile file,
-			@RequestPart("request") @Valid @NotNull UpdateUserRequest request,
-			Authentication authentication) {
-
-		EntityModel<UserDto> model = EntityModel.of(userService.update(file, request, authentication.getName()));
-		model.add(linkTo(methodOn(UserController.class).updateUser(file, request, authentication)).withSelfRel());
-		model.add(linkTo(methodOn(UserController.class).getUser(authentication, authentication.getName()))
-				.withRel("userDetails"));
-		model.add(linkTo(methodOn(UserController.class).updatePassword(authentication)).withRel("updatePassword"));
-		model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).withRel("deleteUser"));
-		model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
-		return ResponseEntity.ok(model);
+	public ResponseEntity<EntityModel<UserDto>> updateUser(@RequestPart("file") @Valid MultipartFile file
+			, Authentication authentication, @RequestPart("request") @Valid @NotNull UpdateUserRequest request) {
+ 
+		return ResponseEntity
+				.ok(EntityModel.of(userService.update(file, request, authentication.getName()),
+					linkTo(methodOn(UserController.class).updateUser(null, null, request)).withSelfRel(),
+					linkTo(methodOn(UserController.class).getUser(null, authentication.getName())).withRel("userDetails"),
+					linkTo(methodOn(UserController.class).updatePassword(null)).withRel("updatePassword"),
+					linkTo(methodOn(UserController.class).deleteUser(null)).withRel("deleteUser"),
+					linkTo(UserController.class).slash("logout").withRel("logout")
+				));
 	}
 
 	@GetMapping("/change_password")
 	public ResponseEntity<EntityModel<UpdatePasswordRequest>> updatePassword(Authentication authentication) {
 
-		EntityModel<UpdatePasswordRequest> model = EntityModel.of(new UpdatePasswordRequest());
-		model.add(linkTo(methodOn(UserController.class).updatePassword(authentication)).withSelfRel());
-		model.add(linkTo(methodOn(UserController.class).getUser(authentication, authentication.getName()))
-				.withRel("userDetails"));
-		model.add(linkTo(methodOn(UserController.class).updateUser(authentication)).withRel("updateUser"));
-		model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).withRel("deleteUser"));
-		model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
-		return ResponseEntity.ok(model);
+		return ResponseEntity
+				.ok(EntityModel.of(new UpdatePasswordRequest(),
+					linkTo(methodOn(UserController.class).updatePassword(null)).withSelfRel(),
+					linkTo(methodOn(UserController.class).getUser(null, authentication.getName())).withRel("userDetails"),
+					linkTo(methodOn(UserController.class).updateUser(null)).withRel("updateUser"),
+					linkTo(methodOn(UserController.class).deleteUser(null)).withRel("deleteUser"),
+					linkTo(UserController.class).slash("logout").withRel("logout")
+				));
 	}
 
-	@PatchMapping("/change_password")
+	/*@PatchMapping("/change_password")
 	public ResponseEntity<EntityModel<UpdatePasswordRequest>> updatePassword(Authentication authentication,
 			@RequestBody UpdatePasswordRequest updatePasswordRequest) {
 
@@ -154,16 +147,17 @@ public class UserController {
 		model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).withRel("deleteUser"));
 		model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
 		return ResponseEntity.ok(model);
-	}
+	}*/
 
 	@PostMapping("/change_password")
-	public void updatePassword(HttpServletRequest request, HttpServletResponse response, Authentication authentication,
+	public ResponseEntity<?> updatePassword(HttpServletRequest request, HttpServletResponse response, Authentication authentication,
 			@RequestBody UpdatePasswordRequest updatePasswordRequest) {
 
-		userService.updatePassword(updatePasswordRequest, authentication.getName());
 		new SecurityContextLogoutHandler().logout(request, response, authentication);
 		try {
 			new UserLogoutSuccessHandler().onLogoutSuccess(request, response, authentication);
+			userService.updatePassword(updatePasswordRequest, authentication.getName());
+			return ResponseEntity.ok().build();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -171,9 +165,10 @@ public class UserController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 
-	@PostMapping("/deleter")
+	/*@PostMapping("/deleter")
 	public ResponseEntity<EntityModel<DeleteUserRequest>> deleteUser(Authentication authentication, String username) {
 		EntityModel<DeleteUserRequest> model = EntityModel.of(new DeleteUserRequest());
 		model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).withSelfRel());
@@ -183,28 +178,30 @@ public class UserController {
 		model.add(linkTo(methodOn(UserController.class).updatePassword(authentication)).withRel("updatePassword"));
 		model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
 		return ResponseEntity.ok(model);
-	}
+	}*/
 
 	@GetMapping("/delete")
 	public ResponseEntity<EntityModel<DeleteUserRequest>> deleteUser(Authentication authentication) {
-		EntityModel<DeleteUserRequest> model = EntityModel.of(new DeleteUserRequest());
-		model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).withSelfRel());
-		model.add(linkTo(methodOn(UserController.class).getUser(authentication, authentication.getName()))
-				.withRel("userDetails"));
-		model.add(linkTo(methodOn(UserController.class).updateUser(authentication)).withRel("updateUser"));
-		model.add(linkTo(methodOn(UserController.class).updatePassword(authentication)).withRel("updatePassword"));
-		model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
-		return ResponseEntity.ok(model);
+		
+		return ResponseEntity
+				.ok(EntityModel.of(new DeleteUserRequest(),
+					linkTo(methodOn(UserController.class).deleteUser(null)).withSelfRel(),
+					linkTo(methodOn(UserController.class).getUser(null, authentication.getName())).withRel("userDetails"),
+					linkTo(methodOn(UserController.class).updateUser(null)).withRel("updateUser"),
+					linkTo(methodOn(UserController.class).updatePassword(null)).withRel("updatePassword"),
+					linkTo(UserController.class).slash("logout").withRel("logout")
+				));
 	}
 
 	@PostMapping("/delete")
-	public void deleteUser(HttpServletRequest request, HttpServletResponse response, Authentication authentication,
+	public ResponseEntity<?> deleteUser(HttpServletRequest request, HttpServletResponse response, Authentication authentication,
 			@RequestBody DeleteUserRequest deleteUserRequest) {
 
-		userService.delete(authentication.getName(), deleteUserRequest.getPassword());
 		new SecurityContextLogoutHandler().logout(request, response, authentication);
 		try {
 			new UserLogoutSuccessHandler().onLogoutSuccess(request, response, authentication);
+			userService.delete(authentication.getName(), deleteUserRequest.getPassword());
+			return ResponseEntity.ok().build();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -212,43 +209,19 @@ public class UserController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 
 	@GetMapping("/user/{username}")
 	public ResponseEntity<EntityModel<UserDto>> getUser(Authentication authentication,
 			@PathVariable("username") String username) {
 
-		/*
-		 * EntityModel<UserDto> model =
-		 * EntityModel.of(userMapper.toUserDto(userService.getUser(username)));
-		 * model.add(linkTo(methodOn(UserController.class).getUser(authentication,
-		 * username)).withSelfRel());
-		 * model.add(linkTo(methodOn(UserController.class).deleteUser(authentication)).
-		 * withRel("deleteUser"));
-		 * model.add(linkTo(methodOn(UserController.class).updateUser(authentication)).
-		 * withRel("updateUser"));
-		 * model.add(linkTo(methodOn(UserController.class).updatePassword(authentication
-		 * )).withRel("updatePassword"));
-		 * model.add(linkTo(UserController.class).slash("logout").withRel("logout"));
-		 */
-		/*
-		 * var methodInvocation = methodOn(UserController.class).getUser(authentication,
-		 * username);
-		 * 
-		 * var link = Affordances.of(linkTo(methodInvocation).withSelfRel())
-		 * 
-		 * .afford(HttpMethod.POST) .withInputAndOutput(UserController.class) //
-		 * .withName("createEmployee") //
-		 * .withTarget(linkTo(UserController.class).slash("newlink").withRel("linkkkk"))
-		 * 
-		 * .toLink();
-		 */
 		return repo.findByUsername(username).map(user -> EntityModel.of(userMapper.toUserDto(user),
 				linkTo(methodOn(UserController.class).getUser(authentication, user.getUsername())).withSelfRel()
 						.andAffordance(
-								afford(methodOn(UserController.class).updateUser(null, new UpdateUserRequest(), null)))
+								afford(methodOn(UserController.class).updateUser(null, null, new UpdateUserRequest())))
 						.andAffordance(afford(
-								methodOn(UserController.class).updatePassword(null, new UpdatePasswordRequest()))),
+								methodOn(UserController.class).updatePassword(null, null, null, new UpdatePasswordRequest()))),
 				linkTo(UserController.class).slash("logout").withRel("logout"))).map(ResponseEntity::ok) //
 				.orElse(ResponseEntity.notFound().build());
 	}
